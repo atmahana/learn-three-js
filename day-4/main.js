c519f5fa1a721f50678c6715462048ae0482040a
Adham <zubairadham.ar@gmail.com>
2023-05-12 14:53:56 +0800

    change model

--- day-4/main.js
+++ day-4/main.js
@@ -1,19 +1,17 @@
 import * as THREE from "three";
 import WebGL from "three/addons/capabilities/WebGL.js";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
-import * as dat from "dat.gui";
 
 function init() {
-  const loadingContainer = document.querySelector(".wrapper");
-  console.log(loadingContainer);
 
   const scene = new THREE.Scene();
   scene.fog = new THREE.FogExp2(0xcccccc, 0);
+  scene.background = new THREE.Color(0xa8def0);
 
   const camera = new THREE.PerspectiveCamera(
-    90,
+    70,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
@@ -29,51 +27,44 @@ function init() {
   let element = document.getElementById("main");
   element.appendChild(renderer.domElement);
 
-  const planeGeo = new THREE.BoxGeometry(1, 0.1, 1);
-  const planeMat = new THREE.MeshPhongMaterial();
-  const plane = new THREE.Mesh(planeGeo, planeMat);
-  scene.add(plane);
+  function generateFloor() {
+    const floorGeo = new THREE.BoxGeometry(10, 0.05, 10);
+    const floorMat = new THREE.MeshPhongMaterial({  color: 0x737373 });
+    const floor = new THREE.Mesh(floorGeo, floorMat);
+    floor.receiveShadow = true;
+    scene.add(floor);
+  }
 
   // Camera control
   const controls = new OrbitControls(camera, renderer.domElement);
-  camera.position.set(0, 2, 0);
+  camera.position.set(0.5, 2, 3);
+  controls.target.set(0,1,0)
   controls.enableDamping = true;
+  controls.minDistance = 1;
+  controls.maxDistance = 5;
+  controls.maxPolarAngle = Math.PI / 3;
   controls.update();
 
-  function skybox() {
-    const skyGeo = new THREE.SphereGeometry(500, 60, 40);
-    // invert the geometry on the x-axis so that all of the faces point inward
-    skyGeo.scale(-1, 1, 1);
-    const skyTexture = new THREE.TextureLoader().load("/sky.jpg");
-    skyTexture.colorSpace = THREE.SRGBColorSpace;
-    const skyMat = new THREE.MeshBasicMaterial({
-      map: skyTexture,
-    });
-    const sky = new THREE.Mesh(skyGeo, skyMat);
-    scene.add(sky);
-  }
-
-  function directLight(){
-    const dl = new THREE.DirectionalLight(0xFFFFFF, 1);
-    const dlHelper = new THREE.DirectionalLightHelper(dl, 2);
-    dl.position.set(0, 3, 5);
+  function directLight() {
+    const dl = new THREE.DirectionalLight(0xffffff, 2);
+    const dlHelper = new THREE.DirectionalLightHelper(dl, 1);
+    dl.castShadow = true;
+    dl.position.set(0, 5, 2);
     dl.target.position.set(0, 0, 0);
+    dlHelper.update();
     scene.add(dl);
     scene.add(dlHelper);
   }
 
   function hemisphereLight() {
-    // Hemisphere light for softer looking scene
-    const hl = new THREE.HemisphereLight(0x17203f, 0x080820, 0.5);
-    const hlHelper = new THREE.HemisphereLightHelper(hl, 2, 0xffffff);
-
+    const hl = new THREE.HemisphereLight(0xffffff, 0x080820, 2);
+    const hlHelper = new THREE.HemisphereLightHelper(hl, 1);
     hl.position.set(0, 5, 0);
     scene.add(hl);
     scene.add(hlHelper);
   }
 
-  let mixer;
-  // Load 3D models
+  let mixer, characterControls;
   function loadModel() {
     const loader = new GLTFLoader();
     const dracoLoader = new DRACOLoader();
@@ -82,26 +73,26 @@ function init() {
     loader.setDRACOLoader(dracoLoader);
 
     loader.load(
-      "/knight.glb",
+      "/Soldier.glb",
       (model) => {
-        const knight = model.scene;
-        knight.traverse((child) => {
-          if (child.isMesh && child.name == "Cube") child.visible = false;
-          if (child.isMesh) child.castShadow = true;
+        const gltf = model.scene;
+        gltf.traverse((child) => {
+          if (child.isMesh) {
+            child.castShadow = true;
+          }
         });
-        mixer = new THREE.AnimationMixer(knight);
-        const clips = model.animations;
-        // const clip = THREE.AnimationClip.findByName(clips, 'Dance')
-        // const action = mixer.clipAction(clip);
-        // action.play();
+        scene.add(gltf);
 
-        clips.forEach(clip => {
-          const name = clip.name.toLowerCase();
-          const action = mixer.clipAction(clip);
+        mixer = new THREE.AnimationMixer(gltf);
+        const animations = model.animations;
+        
+        let animationsMap = new Map();
+        let filter = animations.filter((clip) => clip.name != "TPose");
+        filter.forEach(clip => {
+          animationsMap.set(clip.name, mixer.clipAction(clip));
+          let action = mixer.clipAction(clip);
           action.play();
         });
-
-        scene.add(knight);
       },
       undefined,
       (err) => {
@@ -109,7 +100,7 @@ function init() {
       }
     );
   }
-  
+
   window.addEventListener(
     "resize",
     () => {
@@ -122,14 +113,14 @@ function init() {
 
   const clock = new THREE.Clock();
   function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-    controls.update();
     if(mixer) mixer.update(clock.getDelta());
+    controls.update()
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
   }
   directLight();
   hemisphereLight();
-  skybox();
+  generateFloor();
   loadModel();
   animate();
 }
